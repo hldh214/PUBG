88589d03db455542705d08d3ad82ea1ea190a954
Jim <hldh214@gmail.com>
2018-02-24 19:21:50 +0800

    fix plane.pkl issue

--- pubg_level_down.py
+++ pubg_level_down.py
@@ -8,19 +8,30 @@ from pywinauto import Application as Pwa_app
 from pywinauto.keyboard import SendKeys
 
 
-def image_compare(source_str, target_pic):
-    target = target_pic.convert('L')
+def image_compare(source_str, target_pic, mode='L'):
+    target = target_pic.convert(mode)
     target_size = target.size
     target_img_arr = target.load()
     target_cur_str = ''
-    for x in range(target_size[0]):
-        for y in range(target_size[1]):
-            if target_img_arr[x, y] < 192:
-                target_cur_str += '0'
-            else:
-                target_cur_str += '1'
+    if mode == 'L':
+        for x in range(target_size[0]):
+            for y in range(target_size[1]):
+                if target_img_arr[x, y] < 192:
+                    target_cur_str += '0'
+                else:
+                    target_cur_str += '1'
 
-    return Levenshtein.ratio(source_str, target_cur_str)
+        return Levenshtein.ratio(source_str, target_cur_str)
+    elif mode == 'RGB':
+        for x in range(target_size[0]):
+            for y in range(target_size[1]):
+                # (R, G, B)
+                if target_img_arr[x, y][2] < 50:
+                    target_cur_str += '0'
+                else:
+                    target_cur_str += '1'
+
+        return Levenshtein.ratio(source_str, target_cur_str)
 
 
 class Actions:
@@ -53,7 +64,7 @@ hwnd = FindWindow('UnrealWindow', None)
 window_rect = [each for each in GetWindowRect(hwnd)]
 
 start_rect = list(map(lambda x: x[0] + x[1], zip(window_rect, [50, 680, -1100, -30])))
-plane_rect = list(map(lambda x: x[0] + x[1], zip(window_rect, [205, 600, -1060, -70])))
+plane_rect = list(map(lambda x: x[0] + x[1], zip(window_rect, [214, 600, -1070, -70])))
 reconnect_rect = list(map(lambda x: x[0] + x[1], zip(window_rect, [590, 420, -590, -310])))
 
 pwa_app = Pwa_app().connect(handle=hwnd)
@@ -66,7 +77,7 @@ while 1:
         actions.start()
         continue
 
-    if image_compare(pickle.load(open('dicts/plane.pkl', 'rb')), ImageGrab.grab(plane_rect)) > 0.9:
+    if image_compare(pickle.load(open('dicts/plane.pkl', 'rb')), ImageGrab.grab(plane_rect), 'RGB') > 0.8:
         logging.info('quit')
         actions.quit()
         continue
